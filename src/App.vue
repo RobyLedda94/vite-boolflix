e3b679ca3a9a170bb6148765606eee183c7e9e08
RobyLedda94 <robyledda01@yahoo.it>
2024-08-26 15:05:54 +0200

    [UPD] importate le componenti create nel file app, creato file store con messaggio di prova

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,35 @@
 <script>
+// importo le componenti
+import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
+import FilmList from './components/FilmList.vue'
+import { store } from './store.js'
+
+
+
+
+
 export default {
-  
+  // utilizzo le componenti
+  components: {
+    AppHeader,
+    AppMain,
+    FilmList
+  },
+  data() {
+    return {
+      store,
+    }
+  }
 }
 </script>
 <template lang="">
-  <div>
-    
+  <div class='container'>
+    <div class='row'>
+      <div class='col-12 col-md-4'>
+        {{store.msg}}
+      </div>
+    </div>
   </div>
 </template>
 
---
+++ src/store.js
@@ -0,0 +1,9 @@
+// importare la componente reactive di vue
+
+import { reactive } from 'vue';
+
+// definire costante store da esportare
+
+export const store = reactive({
+    msg: "Prova"
+});
