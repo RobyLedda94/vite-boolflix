9c3cc0442eb86bd6e276a9dd12cb8dbe633d51fb
RobyLedda94 <robyledda01@yahoo.it>
2024-08-27 18:28:31 +0200

    [UPD] definita variabile hover, e le condizioni ch permettono alla descrizione di comparire al passaggio del mouse sulla card, da definire il layout

--- src/components/AppSearchFilms.vue
+++ src/components/AppSearchFilms.vue
@@ -6,22 +6,33 @@ export default {
     props: {
         FilmCard: Object
     },
+    data() {
+        return {
+            // variabile hover settata a falso (non sono sull'immagine)
+            hover: false,
+        };
+    },
 }
 </script>
 <template lang="">
     <div class="col-12 col-sm-6 col-lg-3">
-        <div class="card-films p-2 my-2 text-center bg-light">
-            <img :src="`https://image.tmdb.org/t/p/w342/${FilmCard.poster_path}`" class="img-fluid">
-            <p>
-                {{FilmCard.original_title}}
+        <div class="card-films p-2 my-2 text-center" @mouseover="hover = true" @mouseleave="hover = false">
+            <!-- copertina + info base -->
+             <!-- condizione v-if la varibile hover è falsa percio resta l'immagine -->
 
-            </p>
-            <p>
-                {{FilmCard.original_language}}
-            </p>
-            <p>
-                {{FilmCard.vote_average}}
-            </p>
+            <div v-if="!hover" class="content">
+                <img :src="`https://image.tmdb.org/t/p/w342/${FilmCard.poster_path}`" class="img-fluid">
+            </div>
+            <!-- descrizione -->
+             <!-- se la condizione e vera compare la descrizione -->
+
+             <div v-if="hover" class="card-description">
+                <p>{{FilmCard.original_title}}</p>
+                <p>{{FilmCard.original_language}}</p>
+                <p>{{FilmCard.vote_average}}</p>
+                <p>{{FilmCard.overview}}</p>
+             </div>
+
         </div>
     </div>
 </template>
--- src/components/AppSearchSeries.vue
+++ src/components/AppSearchSeries.vue
@@ -4,26 +4,37 @@
 export default {
     props : {
         SerieCard: Object
-    }
+    },
+    data() {
+        return {
+            // variabile hover settata a falso (non sono sull'immagine)
+            hover: false,
+        };
+    },
 }
 </script>
 <template lang="">
     <div class="col-12 col-sm-6 col-lg-3">
-        <div class="card-serie p-2 my-2 text-center bg-light">
-            <img :src="`https://image.tmdb.org/t/p/w342/${SerieCard.poster_path}`" class="img-fluid">
-            <p>
-                {{SerieCard.original_name}}
-            </p>
-            <p>
-                {{SerieCard.original_language}}
-            </p>
-            <p>
-                {{SerieCard.vote_average}}
-            </p>
+        <div class="card-serie my-2 text-center" @mouseover="hover = true" @mouseleave="hover = false">
+            <!-- copertina + info base -->
+             <!-- condizione v-if la varibile hover è falsa percio resta l'immagine -->
+            <div v-if="!hover" class="content">
+                <img :src="`https://image.tmdb.org/t/p/w342/${SerieCard.poster_path}`" class="img-fluid">
+            </div>
+            <!-- descrizione -->
+             <!-- se la condizione e vera compare la descrizione -->
+             <div v-if="hover" class="card-description">
+                <p>{{SerieCard.original_name}}</p>
+                <p>{{SerieCard.original_language}}</p>
+                <p>{{SerieCard.vote_average}}</p>
+                <p>{{SerieCard.overview}}</p>
+             </div>
         </div>
     </div>
 </template>
 
 <style lang="scss">
+
+
 
 </style>
